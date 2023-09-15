626e627a2a74eeceefb009f4ce9e0ef0151b9c02
Matej Lelek <matej.lelek@deegloo.com>
2023-09-15 15:35:05 +0200

    trigger function config returned

--- azure-functions/try-fetch-and-parse/index.ts
+++ azure-functions/try-fetch-and-parse/index.ts
@@ -1,19 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-     //var timeStamp = new Date().toISOString();
+     var timeStamp = new Date().toISOString();
      
-     //if (myTimer.isPastDue)
-     //{
-     //    context.log('Timer function is running late!');
-     //}
-     //context.log('Timer trigger function ran!', timeStamp);   
-    console.log('Event triggered')
-    context.res = {
-        body: {
-        message: 'Event triggered'
-    }
-    }
+     if (myTimer.isPastDue)
+     {
+         context.log('Timer function is running late!');
+     }
+     context.log('Timer trigger function ran!', timeStamp);   
 };
 
 export default timerTrigger;
---
+++ readme.md
@@ -0,0 +1,2 @@
+## How to setup VSC with Azure for creating and deploying function
+https://www.youtube.com/watch?v=zYb5sVQgUN4&ab_channel=ServerlessonAzure
