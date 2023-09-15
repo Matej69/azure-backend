df6c255492c740a92de4f80751db00411d4fa437
Matej Lelek <matej.lelek@deegloo.com>
2023-09-15 15:17:29 +0200

    init with azure-functions example

--- azure-functions/auth/index.ts
+++ azure-functions/auth/index.ts
@@ -4,12 +4,15 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? " ########## Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {
+            parsed: JSON.stringify(req),
+            message: responseMessage
+        }
     };
 
 };
---
+++ azure-functions/try-fetch-and-parse/index.ts
@@ -0,0 +1,19 @@
+import { AzureFunction, Context } from "@azure/functions"
+
+const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+     //var timeStamp = new Date().toISOString();
+     
+     //if (myTimer.isPastDue)
+     //{
+     //    context.log('Timer function is running late!');
+     //}
+     //context.log('Timer trigger function ran!', timeStamp);   
+    console.log('Event triggered')
+    context.res = {
+        body: {
+        message: 'Event triggered'
+    }
+    }
+};
+
+export default timerTrigger;
