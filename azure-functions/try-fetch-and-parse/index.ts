88620c1e850834235b15e860052650ecaa0e2636
Matej Lelek <matej.lelek@deegloo.com>
2023-09-15 18:27:38 +0200

    123

--- azure-functions/try-fetch-and-parse/index.ts
+++ azure-functions/try-fetch-and-parse/index.ts
@@ -1,13 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-     var timeStamp = new Date().toISOString();
+     //var timeStamp = new Date().toISOString();
      
-     if (myTimer.isPastDue)
-     {
-         context.log('Timer function is running late!');
-     }
-     context.log('Timer trigger function ran!', timeStamp);   
+     //if (myTimer.isPastDue)
+     //{
+     //    context.log('Timer function is running late!');
+     //}
+     //context.log('Timer trigger function ran!', timeStamp);   
 };
 
 export default timerTrigger;
