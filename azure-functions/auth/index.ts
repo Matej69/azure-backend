ae361dfd2a48463c39bd13782f0d21a4531f0f38
Matej Lelek <matej.lelek@deegloo.com>
2023-09-20 11:59:31 +0200

    why dude

---
+++ azure-functions/auth/index.ts
@@ -0,0 +1,29 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+async function fetchData(): Promise<string> {      
+    return new Promise(async(resolve, reject) => {
+        const res = await fetch("https://mpr.datamart.ams.usda.gov/services/v1.1/reports/2453?q=report_date=05/19/2023")
+        if(res.ok) {
+            const body = await res.json()
+            return resolve(body)
+        }
+        else
+        resolve('Greška kod dohvatas111')
+    }) 
+}
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const apiData = await fetchData()
+
+        context.res = {
+            body: {
+                apiData
+            }
+        };
+    context.log(`context.res.body = ${context.res.body}`);
+
+};
+
+export default httpTrigger;
