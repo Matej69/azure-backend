f36ddc36e087bfbb4c5424a29dd2c3ced15ee2b8
Matej69 <matej.lelek@deegloo.com>
2023-09-27 01:26:07 +0200

    Update index.ts

--- azure-functions/auth/index.ts
+++ azure-functions/auth/index.ts
@@ -1,11 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { BlobServiceClient, BlockBlobUploadResponse, StorageSharedKeyCredential } from '@azure/storage-blob';
+
 
 async function fetchData(): Promise<string> {      
     return new Promise(async(resolve, reject) => {
-        const res = await fetch("https://mpr.datamart.ams.usda.gov/services/v1.1/reports/2453?q=report_date=05/19/2023")
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
         if(res.ok) {
             const body = await res.json()
-            return resolve(body)
+            resolve(body)
         }
         else
         resolve('Greška kod dohvatas111 ### ')
@@ -15,11 +17,40 @@ async function fetchData(): Promise<string> {
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const apiData = await fetchData()
+    const apiData: string = 'abcdef' // await fetchData()
+
+    const storageAccountName = 'vscfunction';
+    const storageAccountKey = 'nP6l0ahTiXYUW+VhnOPj7tN0AJbXK0ZZMbVuGKz6RQLxs5akPfUUsUy+lSt91V9EiH5qsTTkvAOu+AStDVFdnA==';
+    const containerName = 'my-stuff';
+    const blobName = 'first.json';
+
+    let exception: any = ''
+    try {
+
+        // Create a BlobServiceClient using the StorageSharedKeyCredential
+        const sharedKeyCredential = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+        const blobServiceClient = new BlobServiceClient(
+            `https://${storageAccountName}.blob.core.windows.net`,
+            sharedKeyCredential
+        );
+        
+        // Get a reference to the container
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        
+        // Get a reference to the blob
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+        const blockBlobUploadResponse: BlockBlobUploadResponse = await blockBlobClient.upload(apiData, apiData.length);
+    }
+    catch(e) {
+        exception += e
+    }
+
+
 
         context.res = {
             body: {
-                apiData
+                //apiData,
+                exception
             }
         };
     context.log(`context.res.body = ${context.res.body}`);
